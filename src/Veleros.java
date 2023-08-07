879ece29faec2997abd893fd10437d06fc78cc0c
CristianRidao <cristianridao@gmail.com>
2023-08-07 12:34:14 -0300

    Update Veleros.java
    doble hola

--- src/Veleros.java
+++ src/Veleros.java
@@ -17,7 +17,7 @@ public class Veleros extends Barco {
  
   public void calcularPrecio(int precio){
       System.out.println("hola");
-      System.out.println("Holaaaaa");
+      System.out.println("Holaaaaa hola");
   } 
     
     
